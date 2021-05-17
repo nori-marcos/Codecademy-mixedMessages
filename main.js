af7cec0ad65c9fd0a5674348b7fe1c4fce3acfd6
nori-marcos <marcos.noriyuki.miyata@gmail.com>
2021-05-17 18:00:34 -0300

    Store message components

---
+++ main.js
@@ -0,0 +1,10 @@
+const tarotCards = {
+    time : ['Past', 'Present', 'Future'],
+    seeker : ['You', 'Your Path', 'Your Potential'],
+    problem : ['Situation', 'Action', 'Outcome'],
+    characteristics : ['Mind', 'Body', 'Spirit'],
+    position : ['Upright', 'Reversed'],
+    majorArcana : ['The Fool', 'The Magician', 'The High Priestess', 'The Empress', 'The Emperor', 'The Hierophant', 'The Lovers', 'The Chariot', 'Strength', 'The Hermit', 'Wheel of Fortune', 'Justice', 'The Hanged Man', 'Death', 'Temperance', 'The Devil', 'The Tower', 'The Star', 'The Moon', 'The Sun', 'Judgement', 'The World', ],    
+};
+
+
